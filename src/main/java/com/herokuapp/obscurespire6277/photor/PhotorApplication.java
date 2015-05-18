68cc6670b5f824c033e2682f1ab138bbd929c99d
wjackson <wjackson@switchfly.com>
2015-05-18 05:22:21 +0000

    Framework added

---
+++ src/main/java/com/herokuapp/obscurespire6277/photor/PhotorApplication.java
@@ -0,0 +1,12 @@
+package com.herokuapp.obscurespire6277.photor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotorApplication.class, args);
+    }
+}
