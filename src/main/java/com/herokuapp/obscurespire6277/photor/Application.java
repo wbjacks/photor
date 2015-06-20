3def51a52ff0eaf15563a9d1f8e9e70f4691d9f3
Will Jackson <william.b.jackson.14@dartmouth.edu>
2015-06-19 21:27:18 -0700

    hentai hentai hentai

--- src/main/java/com/herokuapp/obscurespire6277/photor/Application.java
+++ src/main/java/com/herokuapp/obscurespire6277/photor/Application.java
@@ -1,30 +1,9 @@
 package com.herokuapp.obscurespire6277.photor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import spark.Spark;
 
-import java.util.Arrays;
-
-@SpringBootApplication
 public class Application {
-
-    @Autowired
-    FooRepository _fooRepository;
-
-
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-
-        // For debugging and also the getting started guide made me do it:
-        // Also fuck you Amith I'm leaving comments deal w it
-        String[] beanNames = applicationContext.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            // TODO: Move this to logging
-            System.out.println("Bean loaded in ApplicationContext: " + beanName);
-        }
-
+        Spark.get("/", (req, resp) -> "Hello world!");
     }
 }
--- src/main/java/com/herokuapp/obscurespire6277/photor/FooRepository.java
+++
@@ -1,9 +0,0 @@
-package com.herokuapp.obscurespire6277.photor;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface FooRepository extends CrudRepository<FooModel, Long> {
-    List<FooModel> findByName(String name);
-}
