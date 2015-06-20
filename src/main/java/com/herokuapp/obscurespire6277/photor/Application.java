5bf1f95a9b8880450a9e26735c1f9a11371efd54
wjackson <wjackson@switchfly.com>
2015-06-20 21:57:35 +0000

    Fixed pom.xml to use spark instead of spring

--- src/main/java/com/herokuapp/obscurespire6277/photor/Application.java
+++ src/main/java/com/herokuapp/obscurespire6277/photor/Application.java
@@ -1,9 +1,9 @@
 package com.herokuapp.obscurespire6277.photor;
 
-import spark.Spark;
+import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
-        Spark.get("/", (req, resp) -> "Hello world!");
+        get("/", (req, resp) -> "Hello world!");
     }
 }
