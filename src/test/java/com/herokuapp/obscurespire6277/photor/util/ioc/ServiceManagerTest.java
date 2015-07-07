ddf16be8aa8012f56ef2cc042e75145931296149
Will Jackson <william.b.jackson.14@dartmouth.edu>
2015-07-06 21:14:00 -0700

    Added test stub

---
+++ src/test/java/com/herokuapp/obscurespire6277/photor/util/ioc/ServiceManagerTest.java
@@ -0,0 +1,10 @@
+package com.herokuapp.obscurespire6277.photor.util.ioc;
+
+import org.junit.Test;
+
+public class ServiceManagerTest {
+    @Test
+    public void testRegisteredServiceCanBeFetchedFromPetite() {
+        // TODO: (wjackson) implement this?
+    }
+}
