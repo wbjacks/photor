3a8dbb2911e383896701c259932eec87d9e3be75
Amith Ananthram <amith.ananthram@gmail.com>
2015-10-18 23:08:56 -0700

    drop and re-create tables between unit tests

--- src/main/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/HibernateUtil.java
+++ src/main/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/HibernateUtil.java
@@ -11,24 +11,23 @@ public class HibernateUtil {
     private static SessionFactory _sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
-        if (_sessionFactory == null || _sessionFactory.isClosed())
-        {
-            Configuration configuration = new Configuration()
-                    .configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
-            Reflections reflections = new Reflections("com.herokuapp.obscurespire6277.photor.entities");
-            for (Class<? extends HibernateEntity> hibernateEntity : reflections.getSubTypesOf(HibernateEntity.class)) {
-                configuration = configuration.addAnnotatedClass(hibernateEntity);
-            }
-            StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
-            serviceRegistryBuilder.applySettings(configuration.getProperties());
-            ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
-            _sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        Configuration configuration = new Configuration()
+                .configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
+        Reflections reflections = new Reflections("com.herokuapp.obscurespire6277.photor.entities");
+        for (Class<? extends HibernateEntity> hibernateEntity : reflections.getSubTypesOf(HibernateEntity.class)) {
+            configuration = configuration.addAnnotatedClass(hibernateEntity);
         }
+        StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+        serviceRegistryBuilder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+        _sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         return _sessionFactory;
     }
 
     public static SessionFactory getSessionFactory() {
-        buildSessionFactory();
+        if (_sessionFactory == null || _sessionFactory.isClosed()) {
+            buildSessionFactory();
+        }
         return _sessionFactory;
     }
 
--- src/test/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/TransactorImplTest.java
+++ src/test/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/TransactorImplTest.java
@@ -1,5 +1,6 @@
 package com.herokuapp.obscurespire6277.photor.platform.hibernate;
 
+import com.herokuapp.obscurespire6277.photor.PersistentTestBase;
 import com.herokuapp.obscurespire6277.photor.entities.User;
 import org.junit.Test;
 
@@ -9,14 +10,12 @@ import static com.herokuapp.obscurespire6277.photor.util.time.TimeZones.PT;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-public class TransactorImplTest {
-
-    Transactor transactor = new TransactorImpl();
+public class TransactorImplTest extends PersistentTestBase {
 
     @Test
     public void save_persistsAndReturnsIdOfEntity() {
-        final Id<User> testUserId = transactor.save(new User("test", ZonedDateTime.now(PT)));
-        transactor.execute(new WithVoidReadOnlySession() {
+        final Id<User> testUserId = _transactor.save(new User("test", ZonedDateTime.now(PT)));
+        _transactor.execute(new WithVoidReadOnlySession() {
             @Override
             public void run(TypeSafeSessionWrapper readOnlySession) {
                 User testUser = readOnlySession.getOrThrow(User.class, testUserId);
@@ -27,14 +26,14 @@ public class TransactorImplTest {
 
     @Test
     public void execute_withReadOnlySession_doesNotUpdateDb() {
-        final Id<User> testUserId = transactor.save(new User("test", ZonedDateTime.now(PT)));
-        transactor.execute(new WithVoidReadOnlySession() {
+        final Id<User> testUserId = _transactor.save(new User("test", ZonedDateTime.now(PT)));
+        _transactor.execute(new WithVoidReadOnlySession() {
             @Override
             public void run(TypeSafeSessionWrapper readOnlySession) {
                 readOnlySession.getOrThrow(User.class, testUserId).setHandle("not test");
             }
         });
-        transactor.execute(new WithVoidReadOnlySession() {
+        _transactor.execute(new WithVoidReadOnlySession() {
             @Override
             public void run(TypeSafeSessionWrapper readOnlySession) {
                 assertEquals("test", readOnlySession.getOrThrow(User.class, testUserId).getHandle());
@@ -44,14 +43,14 @@ public class TransactorImplTest {
 
     @Test
     public void execute_withSession_doesUpdateDb() {
-        final Id<User> testUserId = transactor.save(new User("test", ZonedDateTime.now(PT)));
-        transactor.execute(new WithVoidSession() {
+        final Id<User> testUserId = _transactor.save(new User("test", ZonedDateTime.now(PT)));
+        _transactor.execute(new WithVoidSession() {
             @Override
             public void run(TypeSafeSessionWrapper session) {
                 session.getOrThrow(User.class, testUserId).setHandle("not test");
             }
         });
-        transactor.execute(new WithVoidReadOnlySession() {
+        _transactor.execute(new WithVoidReadOnlySession() {
             @Override
             public void run(TypeSafeSessionWrapper readOnlySession) {
                 assertEquals("not test", readOnlySession.getOrThrow(User.class, testUserId).getHandle());
@@ -61,9 +60,9 @@ public class TransactorImplTest {
 
     @Test
     public void execute_exceptionThrown_withAnySession_rollsBackTransaction_propagatesException() {
-        final Id<User> testUserId = transactor.save(new User("test", ZonedDateTime.now(PT)));
+        final Id<User> testUserId = _transactor.save(new User("test", ZonedDateTime.now(PT)));
         try {
-            transactor.execute(new WithVoidSession() {
+            _transactor.execute(new WithVoidSession() {
                 @Override
                 public void run(TypeSafeSessionWrapper session) {
                     session.getOrThrow(User.class, testUserId).setHandle("not test");
@@ -74,7 +73,7 @@ public class TransactorImplTest {
         } catch (IllegalArgumentException e) {
             assertEquals("oh no you didnt", e.getMessage());
         }
-        transactor.execute(new WithVoidReadOnlySession() {
+        _transactor.execute(new WithVoidReadOnlySession() {
             @Override
             public void run(TypeSafeSessionWrapper readOnlySession) {
                 assertEquals("test", readOnlySession.getOrThrow(User.class, testUserId).getHandle());
