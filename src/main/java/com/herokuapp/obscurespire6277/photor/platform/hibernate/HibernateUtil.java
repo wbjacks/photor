237a163d5f1e4a938a6afd376510debaec578e99
Amith Ananthram <amith.ananthram@gmail.com>
2015-10-18 22:56:01 -0700

    add guava and reflections helper libraries, no longer hardcode entities into HibernateUtil

--- src/main/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/HibernateUtil.java
+++ src/main/java/com/herokuapp/obscurespire6277/photor/platform/hibernate/HibernateUtil.java
@@ -1,10 +1,10 @@
 package com.herokuapp.obscurespire6277.photor.platform.hibernate;
 
-import com.herokuapp.obscurespire6277.photor.entities.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.reflections.Reflections;
 
 public class HibernateUtil {
 
@@ -14,12 +14,11 @@ public class HibernateUtil {
         if (_sessionFactory == null || _sessionFactory.isClosed())
         {
             Configuration configuration = new Configuration()
-                    .configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"))
-                    .addAnnotatedClass(User.class)
-                    .addAnnotatedClass(Photo.class)
-                    .addAnnotatedClass(Like.class)
-                    .addAnnotatedClass(Comment.class)
-                    .addAnnotatedClass(LogIn.class);
+                    .configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
+            Reflections reflections = new Reflections("com.herokuapp.obscurespire6277.photor.entities");
+            for (Class<? extends HibernateEntity> hibernateEntity : reflections.getSubTypesOf(HibernateEntity.class)) {
+                configuration = configuration.addAnnotatedClass(hibernateEntity);
+            }
             StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
             serviceRegistryBuilder.applySettings(configuration.getProperties());
             ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
